10c9e6bf711d581c66ee8d09510ab8b8e88e7d03
Marjon Cajocon <marjoncajocon08@gmail.com>
2025-03-08 21:40:50 +0800

    update

--- vuejs/base-project/src/main.js
+++ vuejs/base-project/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+
+import './plugin/font-awesome/css/font-awesome.css';
+import './plugin/bs/css/bootstrap.css'
+import './plugin/bs/js/bootstrap.js'
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
--- vuejs/base-project/src/page/Login.vue
+++ vuejs/base-project/src/page/Login.vue
@@ -1,9 +1,24 @@
 <script setup>
+import { ref } from 'vue';
 
+  const test = ref("marjoncajo");
+
+  const testfn = () => {
+    test.value = "marjon cajocon";
+  };
 </script>
 
 <template>
-  here is the template of the login
+  here is the template of the loginfdsfa
+
+  <div class="w3-card" style="padding: 20px;">
+    <i class="fa fa-plus"></i>
+    <input type="text" v-model="test" class="form-control" />
+
+    <button class="btn btn-success" @click="testfn">
+      <i class="fa fa-sign-in"></i> {{ test }}
+    </button>
+  </div>
 </template>
 
 <style scoped>
