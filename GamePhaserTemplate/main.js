5bd8a8be49c51ae823b07018c1bae46a4cf65b03
Marjon Cajocon <marjoncajocon08@gmail.com>
2025-10-25 21:55:08 +0800

    sample switch scene

--- GamePhaserTemplate/main.js
+++ GamePhaserTemplate/main.js
@@ -1,7 +1,9 @@
 import Phaser from "./phaser.js";
 
 class Game extends Phaser.Scene {
-
+  constructor() {
+    super({key: "g1"});
+  }
   preload() {
       this.load.setBaseURL("http://localhost:8888");
       
@@ -17,6 +19,10 @@ class Game extends Phaser.Scene {
       
       // Define position to move the rectangle
       this.rectanglePositionY = 0;
+
+      setTimeout(() => {
+        this.scene.start("g2");
+      }, 5000);
 
   }
 
@@ -37,16 +43,44 @@ class Game extends Phaser.Scene {
 }
 
 class Game2 extends Phaser.Scene {
+  constructor() {
+    super({key: "g2"});
+  }
   preload() {
+      this.load.setBaseURL("http://localhost:8888");
+      
       
   }
 
   create() {
+      console.log("created game1");
+
+      this.rectangle = this.add.graphics();
+      this.rectangle.fillStyle(0x00FF00, 1); // Blue color with full opacity
+      this.rectangle.fillRect(100, 0, 200, 50); // X, Y, width, height
+      
+      // Define position to move the rectangle
+      this.rectanglePositionY = 0;
+
+      setTimeout(() => {
+        this.scene.start("g1");
+      }, 3000);
 
   }
 
   update() {
+    console.log("trychecking the update");
+  
+    this.rectanglePositionY += 0.3; // Change this value to adjust speed
+    if (this.rectanglePositionY > this.cameras.main.height) {
+        this.rectanglePositionY = 0; // Reset to top once it moves off screen
+    }
 
+    // Clear the graphics and redraw the rectangle at the new position
+    this.rectangle.clear();
+    this.rectangle.fillStyle(0x00FF00, 1);
+    this.rectangle.fillRect(100, this.rectanglePositionY, 200, 50);
+  
   }
 }
 
@@ -54,7 +88,7 @@ const config = {
   type: Phaser.AUTO,
   width: 800,  
   height: 450,
-  scene: [Game],
+  scene: [Game, Game2],
   physics: {
       default: 'arcade',
       arcade: {
